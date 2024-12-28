fef6d8de83ee29d609f51fd1bb35d8f3c9a43d7c
imranhc0 <imranhosein.cse@gmail.com>
2024-12-28 17:24:10 +0600

    added destination directory

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    distDir: 'build', // Specify your custom output directory here
+};
 
 export default nextConfig;
